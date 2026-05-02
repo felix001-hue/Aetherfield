74efbba30a80672b03319cf52fd4f82e65363f60
felix001-hue <felixmokolade@gmail.com>
2026-05-02 18:13:58 +0100

    Anterfield website

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const mainNav = document.getElementById("mainNav");
+const mobileMenu = document.getElementById("mobileMenu");
+
+function toggleMenu() {
+  mobileMenu.classList.remove("translate-x-full");
+  //   mainNav.classList.add("hidden");
+  //   mobileMenu.classList.remove("hidden");
+}
+
+function closeMenu() {
+  mobileMenu.classList.add("translate-x-full");
+  //   mainNav.classList.remove("hidden");
+  //   mobileMenu.classList.add("hidden");
+}
