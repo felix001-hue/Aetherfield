9a7f7ed4c17bd7eeb649e1227e7d6bc7c95bd2e2
felix001-hue <felixmokolade@gmail.com>
2026-05-05 11:08:29 +0100

    navigation

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const mobileMenu = document.getElementById("mobileMenu");
 
 function toggleMenu() {
   mobileMenu.classList.remove("translate-x-full");
+
   //   mainNav.classList.add("hidden");
   //   mobileMenu.classList.remove("hidden");
 }
@@ -13,3 +14,13 @@ function closeMenu() {
   //   mainNav.classList.remove("hidden");
   //   mobileMenu.classList.add("hidden");
 }
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    mainNav.classList.remove("bg-transparent");
+    mainNav.classList.add("bg-white/30", "backdrop-blur-md", "shadow-md", "border-b", "border-white/20", "rounded-2xl", "mx-4", "mt-2");
+  } else {
+    mainNav.classList.remove("bg-white/30", "backdrop-blur-md", "shadow-md", "border-b", "border-white/20", "rounded-2xl", "mx-4", "mt-2");
+    mainNav.classList.add("bg-transparent");
+  }
+});
